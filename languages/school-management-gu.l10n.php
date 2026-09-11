fdb2c75860e767e08f02000975d191110595dc78
mimmdmahfuj <133868399+mimmdmahfuj@users.noreply.github.com>
2026-09-11 12:52:18 +0600

    Add files via upload

---
+++ languages/school-management-gu.l10n.php
@@ -0,0 +1,2 @@
+<?php
+return ['project-id-version'=>'The School Management','report-msgid-bugs-to'=>'','pot-creation-date'=>'2024-06-08 06:51+0000','po-revision-date'=>'2024-06-08 07:56+0000','last-translator'=>'','language-team'=>'Gujarati','language'=>'gu','plural-forms'=>'nplurals=2; plural=n != 1;','mime-version'=>'1.0','content-type'=>'text/plain; charset=UTF-8','content-transfer-encoding'=>'8bit','x-generator'=>'Loco https://localise.biz/','x-loco-version'=>'2.6.9; wp-6.5.4','x-domain'=>'school-management','messages'=>[]];
